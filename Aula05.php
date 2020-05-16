609979806750b4297e6b51f390733c277f842639
Lucas Santos <62790078+LucasSSouzaSSantos@users.noreply.github.com>
2020-05-16 19:28:17 -0300

    Add files via upload

--- Aula05.php
+++ Aula05.php
@@ -1,7 +1,42 @@
 <?php
 
-$n1 = 2;
-$n2 = 3;
-$s = $n1 + $n2;
+/*@var $n1 int */
+$n1 = $_GET["a"];
+/* @var $n2 int */
+$n2 = $_GET["b"];
+$s1 = $n1  + $n2; 
+echo "SOMA = $s1";
+echo '<br>';
+$s2 = $n1  - $n2;
+echo "SUBTRAÇÃO = $s2";
+echo '<br>';
+$s3 = $n1  * $n2;
+echo "MULTIPLICAÇÃO = $s3";
+echo '<br>';
+$s4 = $n1  / $n2;
+echo "DIVISÃO = $s4";
+echo '<br>';
+$s5 = $n1 % $n2;
+echo "RESTO = $s5";
+echo '<br>';
 
-echo "A soma entre $n1 e $n2 é $s sss";
+/*
+ * inserindo valores de forma personalisada:
+ *
+ *  http://localhost/aula05.php?c=3&b=2
+ * 
+ *  $n3 = $_GET["c"]
+ *  $n4 = $_GET["d"]
+ *  */ 
+
+/*
+
+ * Funções Matemáticas
+ *  abs()               Valor Absoluto
+ *  pow()               Potenciação
+ *  sqrt()              Raiz Quadrada
+ *  round()             Arredondamento
+ *  intval()            Valor Inteiro da Variável
+ *  number_format()     Formatação
+ *  */
+
