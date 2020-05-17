4f38ce474a1cf8153742723872936b604da110cc
Lucas Santos <62790078+LucasSSouzaSSantos@users.noreply.github.com>
2020-05-16 22:07:37 -0300

    Add files via upload

---
+++ Aula06.php
@@ -0,0 +1,59 @@
+<html>
+    
+    <body>
+        <h1>PHP</h1>
+        <?php
+        /* Operadores de atribuição: 
+         *      $a = 1;
+         *      $b = 3;
+         *      $c = $c + 5;  $c += 5;
+         *      $c = $a + $b;
+         *      $b = $b + $a; $b += $a;
+         *      $a = $a + 1; $a += 1; $a++;
+         * 
+         * Operadores de Atribuição
+         *  Adição              $a = $a + $b    $a += $b
+         *  Subtração           $a = $a - $b    $a -= $b
+         *  Multiplicação       $a = $a * $b    $a *= $b
+         *  Divisão             $a = $a / $b    $a /= $b
+         *  Módulo              $a = $a % $b    $a %= $b
+         * Concatenação         $a = $a . $b    $a .= $b
+         * 
+         * Operadores de Incremento
+         * 
+         * Pré-incremento       $a = $a + 1      ++$a
+         * Pós-incremento       $a = $a + 1      $a++
+         * Pré-decremento       $a = $a - 1      --$a
+         * Pós-decremento       $a = $a - 1      $a--
+         * 
+         * Variáveis referenciadas
+         * 
+         * $a = 3;
+         * $b = $a;
+         * $b += 5;
+         * echo $a; ---> 3
+         * echo $b; ---> 8
+         * 
+         * $a = 3;
+         * $b = &$a; b seja uma referência a variável a.
+         * $b += 5;
+         * echo $a; ---> 8
+         * echo $b; ---> 8
+         * 
+         * Variáveis de variáveis
+         * 
+         * $site = "cursoemvideo";
+         * $$site = "cursoPHP";
+         * 
+         * Sifrão na frente de sifrão cria a tal variavel de variavel
+         * 
+         * 
+         * 
+         */
+        
+        ?>
+        
+    </body>
+    
+    
+</html>
---
+++ Aula07.php
@@ -0,0 +1,26 @@
+<?php
+
+/* 
+ * Operadores Relacionais
+ * 
+ * Maior                    $a > $b
+ * Menor                    $a < $b
+ * Maior ou Igual           $a >= $b
+ * Menor ou Igual           $a <= $b
+ * Diferente                $a <> $b ou $a != $b
+ * Igual                    $a == $b
+ * idêntico                 $a ==== $b
+ * 
+ * Operador Unário
+ * expressão ? verdadeiro: falso
+ * $maior = $a > $b? $a : $b
+ * 
+ * Operador Lógico
+ * Operador E     and ou &&
+ * Operador OU    or ou ||
+ * Operador XOU   xor     
+ * Operador Não   not ou !
+ * 
+ * 
+ */
+
