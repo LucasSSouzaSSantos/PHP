94261c712a52a5399f051c4bddabdedb0df2e547
Lucas Santos <62790078+LucasSSouzaSSantos@users.noreply.github.com>
2020-05-19 12:08:07 -0300

    Add files via upload

---
+++ Aula11.php
@@ -0,0 +1,10 @@
+<?php
+
+// repetição com teste lógico no início
+
+$c = 1;
+while ($c <= 10){
+    echo $c;
+    $c++;
+}
+
--- receber09.php
+++ receber09.php
@@ -7,9 +7,28 @@
     </head>
     <body>
         <div>
-            <?php        
+            <?php
+                $c = isset($_GET["c"])?$_GET["c"]:1;
+                $f = isset($_GET["f"])?$_GET["f"]:2;
+                $i = isset($_GET["i"])?$_GET["i"]:1;
+                
+                if($c < $f){
+                    while ($c <= $f){
+                    echo "$c ";
+                    $c += $i;
+                }
+                } else {
+                    while ($c >= $f){
+                        echo "$c ";
+                        $c -= $i;
+                    }
+                }
             
             ?>
+            <form method="get" action="enviar09.html">
+                <input type="submit" value="Voltar" class="botao"/>
+                
+            </form>
             <br/>
         </div>
     </body>
