bab7a2656c709cefa3006aa6cf6e8281d43abb51
Lucas Santos <62790078+LucasSSouzaSSantos@users.noreply.github.com>
2020-05-21 12:35:45 -0300

    Add files via upload

---
+++ POO/Aula01.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ *       Aula 01: Programação Orientada a Objeto
+ *  
+ * > Origem da Orientação a Objetos 
+ *  - Objetivo:
+ *     Aproximar o mundo digital do mundo real
+ *  - Como era a programação ?
+ *      programação de baixo nível
+ *      programação linear
+ *      programação estruturada
+ *      programação modular
+ *      programação orientada a objeto
+ *  - Quem criou ?
+ *      Alan Kay
+ * > Linguagens POO
+ *  - C++
+ *  - Java
+ *  - PHP
+ *  - Python
+ *  - Ruby
+ *  - Visual Basic
+ * 
+ * > Vantagens
+ *  - Confiável: O isolamento entre as partes gera software
+ * seguro. Ao alterar uma parte, nenhuma outra é afetada.
+ *  - Oportuno: Ao dividir tudo em partes, várias delas podem 
+ * ser desenvolvidas em paralelo.
+ *  - Manutenível: Atualizar um software é mais fácil. Uma pequena
+ * modificação vai beneficiar todas as partes que usarem o objeto.
+ *  - Exensível: O software não é estático. Ele deve crscer para 
+ * permanecer útil.
+ *  - Reutilizável: Podemos usar objetos de um sistema que criamos
+ * em outro sistema no futuro.
+ *  - Natural: Mais fácil de entender. Você se preocupa mais na 
+ * funcionalidade do que nos detalhes de implementação.
+ * 
+ * 
+ *  */
+
