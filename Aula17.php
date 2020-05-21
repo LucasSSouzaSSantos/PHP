734d8e48b515422433bc725b939cf7c8716ef2bd
Lucas Santos <62790078+LucasSSouzaSSantos@users.noreply.github.com>
2020-05-20 22:12:57 -0300

    Add files via upload

---
+++ Aula17.php
@@ -0,0 +1,38 @@
+<?php        
+
+// 14.Função strtolower($string): devolve uma string em minuscula
+
+// 15.Função strtoupper($string): devolve uma string em maisculas.
+
+// 16.Função ucfirst($string): Coloca a primeira letra em maiscula
+       
+// 17.Função ucword($string): Coloca a primeira letra de cada palavra 
+// em maiscula
+
+// 18.Função strrev($string): devolve a string na ordem inversa
+
+// 19.Função strpos: Verifica em que posição está uma substring
+// dentro de uma string maior. Se a substring foi maiuscula/minuscula
+// e na string estiver minuscula/maiuscula, a função vai retornar um valor
+// falso.
+
+// 20.Função stripos: é usada para resolver o problema que a função a cima
+// tem.
+
+// 21.Funçaõ substr_count: ele mostra quantas vezes uma substring aparece 
+// dentro da string.
+
+// 22.Função substr: ela faz um fatiamento da string 
+
+// 23.Função str_pad:
+//           str_pad($nome, 30, " ",STR_PAD_RIGHT)
+//           str_pad($nome, 30, " ",STR_PAD_CENTER)
+//           str_pad($nome, 30, " ",STR_PAD_LEFT)
+// 
+
+// 24.Função str_repeat("PHP", 5):
+
+// 25.Função str_replace
+
+
+
