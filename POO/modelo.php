a04ec985cd4113c4155690ecc0dc9600d778372e
Lucas Santos <62790078+LucasSSouzaSSantos@users.noreply.github.com>
2020-05-21 14:56:13 -0300

    Add files via upload

---
+++ POO/modelo.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>Aula 02 - POO </title>
+    </head>
+    <body>
+        <?php
+            
+        ?>
+    </body>    
+</html>
