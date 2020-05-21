1c4e82b6042a2bbec4904fffb2c6ae069de0c8b9
Lucas Santos <62790078+LucasSSouzaSSantos@users.noreply.github.com>
2020-05-21 14:46:02 -0300

    Add files via upload

---
+++ POO/Aula02-Teoria.php
@@ -0,0 +1,67 @@
+<?php
+
+/*
+ * Aula 02: O que é um Objeto?
+ *   > Definição: Coisa materail ou abstrata que pode ser 
+ * percebida pelos sentidos e descrita por meio das suas 
+ * características, comportamentos e estdo atual.
+ * 
+ *  > Objeto: Caneta Azul
+ *    Para a POO a caneta de cor azul é um objeto, equanto o 
+ * molde é considerada uma classe.
+ *    Uma classe tem que sempre responder a três perguntas
+ *      - Coisas que eu tenho ?(Atributos)
+ *          - Modelo
+ *          - cor
+ *          - Ponta
+ *          - Carga
+ *          - Tampada
+ *      - Coisas que eu faço ? (Métodos)
+ *          - Escrever
+ *          - Rabiscar
+ *          - Pintar
+ *          - Tampar
+ *          - Destampar
+ *      - Como eu estou agora ? (Estado)
+ * + -------------------------------+
+ * | > Classe Caneta                |
+ * +--------------------------------+
+ * |  modelo:     Caractere         |
+ * |  cor:        Caractere         |
+ * |  ponta:      Real              |
+ * |  carga:      Inteiro           |
+ * |  tampada:    Logico            |
+ * +--------------------------------+
+ * |    Metodo rabiscar()           |
+ * |        se (tampada) então      |
+ * |            Escreva("Erro")     |
+ * |        senão                   |
+ * |                                |
+ * |        fimSe                   |
+ * |                                |    
+ * |        fimMetodo               |    
+ * |                                |                
+ * |    Metodo tampar()             |                
+ * |                                |                        
+ * |    fimMetodo                   |            
+ * |                                |                
+ * |                                |                            
+ * |  > FimClasse                   |            
+ * +--------------------------------+
+ * 
+ * Classe
+ *      Definição: Define os atributos e métodos comuns que 
+ * serão compartilhados por um objeto.
+ * 
+ * Objeto
+ *      Definição: É uma instância de uma classe.
+ * 
+ * Abstração:
+ * 
+ * Atividades: 
+ * Escolher dois objetos ao seu lado
+ *  - Cadeira e Ventilador
+ * Classificar dois objetos abstrato
+ *  - 
+ * *  */
+
