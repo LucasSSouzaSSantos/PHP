06b3e92436354fe92283557a508250bd4db8dd0f
Lucas Santos <62790078+LucasSSouzaSSantos@users.noreply.github.com>
2020-05-23 20:07:08 -0300

    Add files via upload

---
+++ POO/Resposta05.php
@@ -0,0 +1,83 @@
+<?php
+
+/* 
+ + -------------------------------------------------- +
+ *                 Minha Resposta                     *
+ + -------------------------------------------------- +
+ * Classe Conta
+ * 
+ *  publico      numConta:    Caractere
+ *  protegido    tipo:        Caractere
+ *  privado      dono:        Caractere
+ *  privado      saldo:       Real 
+ *  privado      status:      lógico
+ * 
+ * Metodo Construtor()
+ *      status = falso
+ *      saldo = 0
+ * FimMetodo
+ * 
+ * publico getNumConta()
+ *      retorne numConta
+ * fimMetodo
+ * 
+ * publico setNumConta(num: caractere)
+ *      numConta = num
+ * fimMetodo
+ * 
+ * publico getTipo()
+ *      retorne tipo
+ * fimMetodo
+ * 
+ * publico setTipo(t: caractere)
+ *      tipo = t
+ * fimMetodo
+ * 
+ * publico getDono()
+ *      retorne Dono
+ * fimMetodo
+ * 
+ * publico setDono(d: caractere)
+ *      dono = d
+ * fimMetodo
+ * 
+ * publico getSaldo()
+ *      retorne saldo
+ * fimMetodo
+ * 
+ * publico setSaldo(sa: Real)
+ *      saldo = sa
+ * fimMetodo
+ * 
+ * publico getStatus()
+ *      retorne status
+ * fimMetodo
+ * 
+ * publico setStatus(s: lógico)
+ *      status = s
+ * fimMetodo
+ * 
+ * publico Metodo abrirConta()
+ * 
+ * fimMetodo
+ * 
+ * publico Metodo fecharConta()
+ * 
+ * fimMetodo
+ * 
+ * publico Metodo depositar()
+ * 
+ * fimMetodo
+ * 
+ * publico Metodo sacar()
+ * 
+ * fimMetodo
+ * 
+ * publico Metodo pagarMensal()
+ * 
+ * fimMetodo
+ * 
+ * fimClasse
+ *  
+ */
+
