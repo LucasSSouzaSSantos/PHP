a16b9977e5f7fed771fd63365ca06831789dc256
Lucas Santos <62790078+LucasSSouzaSSantos@users.noreply.github.com>
2020-05-23 22:01:47 -0300

    Add files via upload

--- POO/Resposta05.php
+++ POO/Resposta05.php
@@ -4,22 +4,24 @@
  + -------------------------------------------------- +
  *                 Minha Resposta                     *
  + -------------------------------------------------- +
- * Classe Conta
- * 
- *  publico      numConta:    Caractere
- *  protegido    tipo:        Caractere
- *  privado      dono:        Caractere
- *  privado      saldo:       Real 
- *  privado      status:      lógico
- * 
- + --------------------------------------------------- +
- *                 Métodos Especiais                   *
- + --------------------------------------------------- +
- *  Metodo Construtor()
- *      status = falso
- *      saldo = 0
- * FimMetodo
- * 
+ *       Inicio(nome e atributos da classe)           *
+ + -------------------------------------------------- +
+ *                  Classe Conta                      *
+ *                                                    *
+ *  publico      numConta:    Caractere               *
+ *  protegido    tipo:        Caractere               *
+ *  privado      dono:        Caractere               *
+ *  privado      saldo:       Real                    *
+ *  privado      status:      lógico                  *
+ *                                                    * 
+ + ---------------------------------------------------+
+ *                 Métodos Especiais                  *
+ + ---------------------------------------------------+
+ *  Metodo Construtor()                               *      
+ *      status = falso                                *  
+ *      saldo = 0                                     *              
+ * FimMetodo                                          *      
+ *                                                    * 
  * publico getNumConta()
  *      retorne numConta
  * fimMetodo
@@ -49,7 +51,7 @@
  * fimMetodo
  * 
  * publico setSaldo(sa: Real)
- *      saldo = sa
+ *      saldo = saldo + sa
  * fimMetodo
  * 
  * publico getStatus()
@@ -59,27 +61,67 @@
  * publico setStatus(s: lógico)
  *      status = s
  * fimMetodo
+ * 
  + --------------------------------------------------- +
  *                    Métodos                          *
  + --------------------------------------------------- +
- * publico Metodo abrirConta()
  * 
+ * publico Metodo abrirConta(t: caractere, s: logico)
+ *      status = s
+ *      setTipo(t)
+ *      se (t == "CC") entao
+ *           setSaldo(50)
+ *      Senão
+ *          setSaldo(150)
+ *      fimSe 
  * fimMetodo
  * 
  * publico Metodo fecharConta()
- * 
+ *      Se (status == verdadeiro) então
+ *          Escreva("Você estar tentando fachar uma conta que não foi aberta")
+ *      Senão
+ *          Se (saldo == 0) então
+ *              Escreva("Conta Fechada Com Sucesso")
+ *              status = falso
+ *          Senão
+ *              Se (saldo > 0) então
+ *                  Escreva("Você tem dinheiro nessa conta, saque antes de fechar")
+ *              Senão
+ *                  Escreva("Você ainda tem débito na conta, pague antes de fechar")
+ *              fimSe
+ *          fimSe          
+ *      fimSe
  * fimMetodo
  * 
- * publico Metodo depositar()
- * 
+ * publico Metodo depositar(valor: Real)
+ *      Se (status == falso) então
+ *          Escreva("Conta ainda não foi aberta, fale com um dos nossos gerentes, e abra sua conta!")
+ *      Senão
+ *          setSaldo(valor) 
  * fimMetodo
  * 
- * publico Metodo sacar()
- * 
+ * publico Metodo sacar(valor: Real)
+ *      Se (status == falso) então
+ *          Escreva("Essa conta ainda não foi aberta, fale com um dos nossos gerente, e abra sua conta")
+ *      Senão
+ *          Se (saldo > 0) então
+ *              Escreva("Você não tem saldo na conta")
+ *          Senão
+ *              Se (valor > saldo) então
+ *                  Escreva("Saldo insuficeinte: ante de proseguir confirme seu saldo")
+ *              Senão
+ *                  setSaldo(-valor)
+ *              fimSe
+ *          fimSe
+ *      fimSe
  * fimMetodo
  * 
  * publico Metodo pagarMensal()
- * 
+ *      Se getTipo() == "CC" então
+ *          setSaldo(-12)
+ *      Senão
+ *          setSaldo(-15)
+ *      fimSe
  * fimMetodo
  * 
  * fimClasse
